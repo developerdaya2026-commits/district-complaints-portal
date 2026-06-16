1ec27635792596b89e4308982b51bc4f54b29f9e
developerdaya2026-commits <developerdaya2026@gmail.com>
2026-06-16 15:41:03 +0530

    Update Complain.py

--- Complain.py
+++ Complain.py
@@ -52,7 +52,7 @@ st.markdown("""
 
 # 2. Configuration & API Endpoints
 GSHEET_URL = "https://docs.google.com/spreadsheets/d/1kQx4dwtKNAQ2mKAvpohbAYLdKCh-nqNqQs8AV6VsGSQ/gviz/tq?tqx=out:csv"
-WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxBmY0o566hGBQERklGQI-Jk_ijXU0NOhf9ZdWUJ3ZuFVVwJ-XKHPwxdTuJu3i_67Mn/exec"
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwbPVxH76ciSmcvjvBUlHEuL-Rxw5Yz6qsrl-amSNnLfFYaCJ7jitXZxeKGL7PhEeK2/exec"
 
 # Static Registry for Authentication
 USER_REGISTRY = {
